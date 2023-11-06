d89a3a2342c525f29c4f023af1b8d5c5cc9c35c5
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2023-11-06 14:12:22 -0300

    Initial Commit

---
+++ NanoProject4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NanoProject4
+//
+//  Created by Gustavo Horestee Santos Barros on 06/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NanoProject4/NanoProject4App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoProject4App.swift
+//  NanoProject4
+//
+//  Created by Gustavo Horestee Santos Barros on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoProject4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
