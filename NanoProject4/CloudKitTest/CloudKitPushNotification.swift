094512e4e8296993a90efff579460d247dad8e53
Lukezin03 <117870645+Lukezin03@users.noreply.github.com>
2023-11-09 15:32:17 -0300

    Consertando bugs

--- NanoProject4/CloudKitTest/CloudKitPushNotification.swift
+++ NanoProject4/CloudKitTest/CloudKitPushNotification.swift
@@ -35,50 +35,51 @@ class ClouKitPushNotificationViewModel: ObservableObject {
             guard success else { return }
             DispatchQueue.main.async {
                 UIApplication.shared.registerForRemoteNotifications()
-        let predicate = NSPredicate(value: true)
-        
-        let subscription = CKQuerySubscription(recordType: "Notification", predicate: predicate, subscriptionID: "Notification_requested", options: .firesOnRecordCreation)
-        
-        let notification = CKSubscription.NotificationInfo()
-        notification.title = "There's a new notification"
-        notification.alertBody = "Check this new restourant"
-        notification.soundName = "default"
-        
-        subscription.notificationInfo = notification
-        
-        CKContainer.default().publicCloudDatabase.save(subscription) { returnedSubscription, returnedError in
-            if let error = returnedError {
-                print(error)
-            } else {
-                print("Succesfully Subscribed to Notifications")
+                let predicate = NSPredicate(value: true)
+                
+                let subscription = CKQuerySubscription(recordType: "Notification", predicate: predicate, subscriptionID: "Notification_requested", options: .firesOnRecordCreation)
+                
+                let notification = CKSubscription.NotificationInfo()
+                notification.title = "There's a new notification"
+                notification.alertBody = "Check this new restourant"
+                notification.soundName = "default"
+                
+                subscription.notificationInfo = notification
+                
+                CKContainer.default().publicCloudDatabase.save(subscription) { returnedSubscription, returnedError in
+                    if let error = returnedError {
+                        print(error)
+                    } else {
+                        print("Succesfully Subscribed to Notifications")
+                    }
+                }
             }
         }
     }
 }
-
-struct CloudKitPushNotification: View {
-
-    @StateObject private var vm = ClouKitPushNotificationViewModel()
-    @StateObject private var pushNotificationManager = PushNotificationManager()
-
-    var body: some View {
-        VStack(spacing: 40) {
-            Button("Request notification permissions") {
-                vm.requestNotificationPermissions()
-            }
-
-            Button("Subscribe to notifications") {
-                vm.subscribeToNotifications()
-            }
-
-            Button("Solicitar Permissões de Notificação") {
-                pushNotificationManager.requestNotificationPermissions()
+    
+    struct CloudKitPushNotification: View {
+        
+        @StateObject private var vm = ClouKitPushNotificationViewModel()
+        @StateObject private var pushNotificationManager = PushNotificationManager()
+        
+        var body: some View {
+            VStack(spacing: 40) {
+                Button("Request notification permissions") {
+                    vm.requestNotificationPermissions()
+                }
+                
+                Button("Subscribe to notifications") {
+                    vm.subscribeToNotifications()
+                }
+                
+                Button("Solicitar Permissões de Notificação") {
+                    pushNotificationManager.requestNotificationPermissions()
+                }
             }
         }
     }
-}
 
 #Preview {
     CloudKitPushNotification()
 }
-
