f0fdc511321b1d967381f02726613768358a4548
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2023-11-10 15:17:06 -0300

    conflitos de murge resolvidos

--- NanoProject4/View/Screens/Home/RestaurantViewModel.swift
+++ NanoProject4/View/Screens/Home/RestaurantViewModel.swift
@@ -25,82 +25,65 @@ class RestaurantViewModel: ObservableObject{
     
     @Published  var selectedImage: UIImage? = nil
     @Published var imageSelection: PhotosPickerItem? = nil{
-            didSet{
-                Task{
-                    try? await setImage(from: imageSelection)
-                }
-            }
-    
-    public var random: RestaurantModel {
-        let idx = Int.random(in: 0...(Restaurants.count-1))
-        let restaurant = Restaurants[idx]
-        return restaurant
-    }
-    
-    func togleSheetAddRest(){
-        self.isOpen.toggle()
-    }
-    
-    func creatNewRestaurant(){
-        print(selectedImage as Any)
-        self.Restaurants.append(RestaurantModel(name: nameRest, description: description, imageRest: selectedImage, locationRest: "Teste", rating: "4.9", isfavorite: false))
-    }
-    
-    private func setImage(from selection: PhotosPickerItem?){
-        guard let selection else { return }
-        
-        Task{
-            if let data = try? await selection.loadTransferable(type: Data.self){
-                if let uiImage = UIImage(data: data){
-                    selectedImage = uiImage
-                }
+        didSet{
+            Task{
+                try? await setImage(from: imageSelection)
             }
         }
-    
-  
-    public func togleSheetAddRest(){
-        self.isOpenSheet.toggle()
     }
-    
-    
-    public func resetVariables(){
-        nameRest = ""
-        description = ""
-        selectedImage = nil
-        imageSelection = nil
-    }
-    
-    
-    public func creatNewRestaurant(){
-        let newRestaurant = RestaurantModel(name: nameRest,
-                                            description: description,
-                                            imageRest: selectedImage,
-                                            locationRest: "Teste",
-                                            rating: "4.9",
-                                            isfavorite: false)
-        self.restaurants.append(newRestaurant)
-    }
-    
-    
-//    public func toogleValueIsfavorite(from restaurant: RestaurantModel){
-//        var imatableRestaurant = restaurant
-//        imatableRestaurant.isfavorite.toggle()
-//    }
-    
-    
-    ////funcao que recebe um fotoPicker e converte em UIImage
-    private func setImage(from selection: PhotosPickerItem?) async throws {
-        guard let selection else { return }
-        do{
-            if let data = try await selection.loadTransferable(type: Data.self){
-                selectedImage = UIImage(data: data)
-            }
-        }catch{
-            print("🚨 -> Error em converter o data do picker para UIImage")
+        
+        public var random: RestaurantModel {
+            let idx = Int.random(in: 0...(restaurants.count-1))
+            let restaurant = restaurants[idx]
+            return restaurant
         }
         
-    }
-}
+        func togleSheetAddRest(){
+            self.isOpenSheet.toggle()
+        }
+      
+            
+            
+            
+            
+            public func resetVariables(){
+                nameRest = ""
+                description = ""
+                selectedImage = nil
+                imageSelection = nil
+            }
+            
+            
+            public func creatNewRestaurant(){
+                let newRestaurant = RestaurantModel(name: nameRest,
+                                                    description: description,
+                                                    imageRest: selectedImage,
+                                                    locationRest: "Teste",
+                                                    rating: "4.9",
+                                                    isfavorite: false)
+                self.restaurants.append(newRestaurant)
+            }
+            
+            
+            //    public func toogleValueIsfavorite(from restaurant: RestaurantModel){
+            //        var imatableRestaurant = restaurant
+            //        imatableRestaurant.isfavorite.toggle()
+            //    }
+            
+            
+            ////funcao que recebe um fotoPicker e converte em UIImage
+            private func setImage(from selection: PhotosPickerItem?) async throws {
+                guard let selection else { return }
+                do{
+                    if let data = try await selection.loadTransferable(type: Data.self){
+                        selectedImage = UIImage(data: data)
+                    }
+                }catch{
+                    print("🚨 -> Error em converter o data do picker para UIImage")
+                }
+                
+            }
+        }
 
 
 //MARK: - Instancia asset de RestaurantModel para teste
@@ -112,18 +95,21 @@ struct RestaurantsAsset{
                          locationRest: "Vicente-Pires",
                          rating: "4.9",
                          isfavorite: false),
+         
         RestaurantModel(name: "Apache Hamburgueria",
                         description: "hamburgueria hambúrguer burguer burger smash blend sanduíche artesanal angus hot dog cachorro quente pizzaria pizza crepe pastel pastelaria esfiha esfirra massas lasanha macarrão bolonhesa peito coxinha da asa asinha coxa e sobrecoxa frango frito assado grelhado feijoada carne de sol picanha costela costelinha fraldinha contra filé mignon strogonoff parrilla maminha alcatra churrascaria churrasco churrasquinho espeto espetinho jantinha janta almoço refeição prato executivo restaurante marmita gourmet acaiteria esuíno suína risoto fettuccine petisco frutos do mar camarão peixe barbecue batata saudável fitness açaí",
                         imageRest: UIImage(resource: .restauranteAsset),
                         locationRest: "Vicente-Pires",
                         rating: "4.9",
                         isfavorite: false),
+         
         RestaurantModel(name: "Restaurante Sabores do Cerrado",
                         description: "Descubra a autêntica culinária do cerrado brasileiro, onde sabores regionais se encontram para uma experiência única.",
                         imageRest: UIImage(resource: .restauranteAsset),
                         locationRest: "SHS Quadra 401, Bloco C, Asa Sul, Brasília",
                         rating: "4.9",
                         isfavorite: false),
+         
         RestaurantModel(name: "Prazeres do Planalto Grill",
                         description: "Carnes suculentas, preparadas com maestria e servidas com um toque regional, celebrando os prazeres do planalto central.",
                         imageRest: UIImage(resource: .restauranteAsset),
