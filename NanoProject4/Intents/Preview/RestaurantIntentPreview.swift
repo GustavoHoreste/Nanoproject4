b47780415bd9933544229329d922ea8274d319f0
fabiohbfreitas <difreitas.fabio@gmail.com>
2023-11-10 14:01:01 -0300

    feat: add siri intent for random restaurant

---
+++ NanoProject4/Intents/Preview/RestaurantIntentPreview.swift
@@ -0,0 +1,8 @@
+//
+//  RestaurantIntentPreview.swift
+//  NanoProject4
+//
+//  Created by Fabio Freitas on 10/11/23.
+//
+
+import Foundation
---
+++ NanoProject4/Intents/Shortcuts/IntentsShortcutProvider.swift
@@ -0,0 +1,8 @@
+//
+//  IntentsShortcutProvider.swift
+//  NanoProject4
+//
+//  Created by Fabio Freitas on 10/11/23.
+//
+
+import Foundation
