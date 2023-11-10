a637631c4364719a23f74e04efeffdeedcc535aa
fabiohbfreitas <difreitas.fabio@gmail.com>
2023-11-10 14:02:10 -0300

    feat: add siri intent for random restaurant

--- NanoProject4/Intents/Preview/RestaurantIntentPreview.swift
+++ NanoProject4/Intents/Preview/RestaurantIntentPreview.swift
@@ -2,7 +2,32 @@
 //  RestaurantIntentPreview.swift
 //  NanoProject4
 //
-//  Created by Fabio Freitas on 10/11/23.
+//  Created by Fabio Freitas on 09/11/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct RestaurantIntentPreview: View {
+    let restaurant: RestaurantModel
+    
+    var body: some View {
+        VStack {
+            if let image = restaurant.imageRest {
+                Image(uiImage: image)
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .clipShape(RoundedRectangle(cornerSize: /*@START_MENU_TOKEN@*/CGSize(width: 20, height: 10)/*@END_MENU_TOKEN@*/))
+            }
+            Text(restaurant.name)
+                .bold()
+                .font(.headline)
+            Text(restaurant.locationRest)
+                .font(.subheadline)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    RestaurantIntentPreview(restaurant: RestaurantViewModel.shared.random)
+}
--- NanoProject4/Intents/Shortcuts/IntentsShortcutProvider.swift
+++ NanoProject4/Intents/Shortcuts/IntentsShortcutProvider.swift
@@ -2,7 +2,15 @@
 //  IntentsShortcutProvider.swift
 //  NanoProject4
 //
-//  Created by Fabio Freitas on 10/11/23.
+//  Created by Fabio Freitas on 09/11/23.
 //
 
-import Foundation
+import SwiftUI
+import AppIntents
+
+struct IntentsShortcutProvider: AppShortcutsProvider {
+    @AppShortcutsBuilder
+    static var appShortcuts: [AppShortcut] {
+        AppShortcut(intent: RandomRestaurantIntent(), phrases: ["Teste de \(.applicationName)"], shortTitle: "Restaurante Aleatório", systemImageName: "rectangle.fill.on.rectangle.angled.fill")
+    }
+}
